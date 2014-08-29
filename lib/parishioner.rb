8040079ec1e75e2e19c0292f8ccdeb7be5975376
Patrick David <Patrick.David3@gmail.com>
2014-08-29 20:15:41 +0000

    create a callback in the parishioner class that will capitalize the first letter of the name

--- lib/parishioner.rb
+++ lib/parishioner.rb
@@ -1,3 +1,12 @@
 class Parishioner < ActiveRecord::Base
+  
+  after_save :capitalize_name
+  
   belongs_to :church
+  
+private
+  
+  def capitalize_name
+    self.name = self.name[0].upcase + self.name[1..-1]
+  end
 end
--- spec/parishioner_spec.rb
+++ spec/parishioner_spec.rb
@@ -5,6 +5,10 @@ describe Parishioner do
     new_church = Church.create({:name => "SJB"})
     new_member = Parishioner.create({:name => "Bob", :church_id => new_church.id})
     new_member1 = Parishioner.create({:name => "Scott", :church_id => new_church.id})
-    new_member1.church.should eq new_church
+    expect(new_member1.church).to eq  new_church
+  end
+  it 'capitalizes the name' do
+    member = Parishioner.create(:name => "joe", :church_id =>1)
+    expect(member.name).to eq "Joe"
   end
 end
